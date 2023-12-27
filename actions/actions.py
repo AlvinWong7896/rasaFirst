74c30ad0d3b7f64e052ab718e9d1bfca424efd14
CK Wong <alvinwong7896@gmail.com>
2023-12-27 12:35:24 +0800

    included entitites and actions

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,58 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class extractFoodEntity(Action):
+    def name(self) -> Text:
+        return "action_extract_food_entity"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        food_entity = next(tracker.get_latest_entity_values("food"), None)
+
+        if food_entity:
+            dispatcher.utter_message(
+                text=f"You have selected {food_entity} as your choice."
+            )
+        else:
+            dispatcher.utter_message(text="I'm sorry, I'd not detect your choice.")
+        return []
+
+
+class orderFoodAction(Action):
+    def name(self) -> Text:
+        return "action_order_food"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text="Sure, which kinda food would you like to order?")
+        return []
+
+
+class confirmOrderAction(Action):
+    def name(self) -> Text:
+        return "action_confirm_order"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        food_entity = next(tracker.get_latest_entity_values("food"), None)
+
+        if food_entity:
+            dispatcher.utter_message(text=f"I've ordered {food_entity} for you.")
+        else:
+            dispatcher.utter_message(text="I'm sorry, I'd not detect your choice.")
+        return []
